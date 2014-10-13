b0a6e38bfc10cd6ac32c324292722a4e05d7b5db
jm2dev <jomarcar@gmail.com>
2014-10-13 22:28:31 +0100

    Implemented using stream api.

--- src/test/java/com/opt1mates/citations/ResponseBuilderTest.java
+++ src/test/java/com/opt1mates/citations/ResponseBuilderTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -33,7 +35,25 @@ public class ResponseBuilderTest {
         assertThat("Plain text for the rest of the world", actual, is(expected));
     }
 
+    @Test
+    public void default_to_plain_text() {
+        String actual = getContentType("WYSWYG");
+        String expected = "application-octet-stream";
+
+        assertThat("Default to ASCII", actual, is(expected));
+    }
+
     private String getContentType(String format) {
+        final Citation RIS = new Citation("RIS", "application/x-research-info-systems");
+        final Citation BIBTEX = new Citation("BIBTEX", "text/x-bibtex");
+        final Citation ASCII = new Citation("ASCII", "application-octet-stream");
+        Stream<Citation> citations = Stream.of(RIS, BIBTEX, ASCII);
+
+        Optional<Citation> optionalCitation = citations.filter(citation -> citation.getFormat().equalsIgnoreCase(format)).findFirst();
+        return optionalCitation.orElse(ASCII).getContentType();
+    }
+
+    private String getOldContentType(String format) {
         Map<String, String> contentTypes = new HashMap<>();
         contentTypes.put("RIS", "application/x-research-info-systems");
         contentTypes.put("BIBTEX", "text/x-bibtex");
@@ -46,5 +66,23 @@ public class ResponseBuilderTest {
             result = "application-octet-stream";
 
         return result;
+    }
+
+    private class Citation {
+        private final String format;
+        private final String contentType;
+
+        private Citation(String format, String contentType) {
+            this.format = format;
+            this.contentType = contentType;
+        }
+
+        public String getFormat() {
+            return format;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
     }
 }
