53fffc4f90d6aa9a747c12a70df5fb25c8fd37fd
jm2dev <jomarcar@gmail.com>
2014-10-13 20:47:42 +0100

    Initial commit.

---
+++ src/test/java/com/opt1mates/citations/ResponseBuilderTest.java
@@ -0,0 +1,10 @@
+package com.opt1mates.citations;
+
+import org.testng.annotations.Test;
+
+public class ResponseBuilderTest {
+    @Test
+    public void ris_application_for_ris_format() {
+
+    }
+}
